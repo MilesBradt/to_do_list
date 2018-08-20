cac4737118e65ac899ac1138f2dbda1c7881f6bb
Josh Gearheart and Michael Bradt <josh+michael@>
2018-08-20 16:26:36 -0700

    remove user added tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// function ToDo(task, id) {
+//   userInput = task;
+//   count = id;
+// }
+
 var count = 0;
 var tasks = [];
 
@@ -5,12 +10,26 @@ $(document).ready(function() {
   $("form#tasks").submit(function(event){
     event.preventDefault();
     $("input#userInput").each(function(){
+      $("#output").show();
       var userInput = $(this).val();
       userInput = userInput.toString();
-      tasks.push(userInput)
-      $("ul#outputTasks").append("<li>" + tasks[count] + "</li>");
+      // var list'+count+' = new ToDo(task, id);
+      tasks.push(userInput);
+      $("ul#outputTasks").append("<li id='item"+count+"'>" + tasks[count] + "&nbsp" + "&nbsp" + "<button id='item"+count+"' class='btn doneBtn'>Done</button>" + "<br>" + "<br>" + "</li>");
       count ++;
-      console.log(tasks);
+
     });
+    $(".doneBtn").click(function(event){
+      console.log(count);
+      var taskIndex = $(this).attr("id");
+      $("li#"+taskIndex).hide();
+      console.log(taskIndex);
+      // $('li#item'+count).hide();
+    });
+
   });
 });
+
+
+'item"+count+"'
+"li#' + taskIndex+'"
