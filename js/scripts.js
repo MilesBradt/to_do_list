5544e1078005a49e5ba2902d86039ed28c73dee0
Josh Gearheart and Michael Bradt <josh+michael@>
2018-08-20 14:43:10 -0700

    take user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
+var toDo = 0;
+var tasks = [];
 
+$(document).ready(function() {
+  $("form#tasks").submit(function(event){
+    event.preventDefault();
+    $("input#userInput").each(function(){
+      var userInput = $(this).val();
+      console.log(userInput);
+    });
+  });
 });
